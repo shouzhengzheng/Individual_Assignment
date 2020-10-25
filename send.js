f25ce248687e750735061511b4fc87344c004681
limshouzheng <limshouzheng@gmail.com>
2020-10-25 10:47:52 +0800

    First Commit

---
+++ send.js
@@ -0,0 +1,23 @@
+function sendRayaWhatsapp(){
+    inputName = document.getElementById("name").value;
+
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Selamat%20Hari%20Raya%0Awww.google.com';
+}
+
+function sendCNYWhatsapp(){
+    inputName = document.getElementById("name").value;
+
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Chinese%20New%20Year!%0Awww.google.com';
+}
+
+function sendChristmasWhatsapp(){
+    inputName = document.getElementById("name").value;
+
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Merry%20Christmas%0Awww.google.com';
+}
+
+function sendDeepavaliWhatsapp(){
+    inputName = document.getElementById("name").value;
+
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Deepavali%0Awww.google.com';
+}
