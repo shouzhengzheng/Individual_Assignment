5776a99fc5edb12facc5bb7db839c5ca0b98acd7
limshouzheng <limshouzheng@gmail.com>
2020-10-25 11:35:48 +0800

    change javascript1

--- send.js
+++ send.js
@@ -1,23 +1,23 @@
 function sendRayaWhatsapp(){
     inputName = document.getElementById("name").value;
 
-    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Selamat%20Hari%20Raya%0Awww.google.com';
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Selamat%20Hari%20Raya%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/hariraya.html';
 }
 
 function sendCNYWhatsapp(){
     inputName = document.getElementById("name").value;
 
-    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Chinese%20New%20Year!%0Awww.google.com';
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Chinese%20New%20Year!%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/happynewyear.html';
 }
 
 function sendChristmasWhatsapp(){
     inputName = document.getElementById("name").value;
 
-    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Merry%20Christmas%0Awww.google.com';
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Merry%20Christmas%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/christmas.html';
 }
 
 function sendDeepavaliWhatsapp(){
     inputName = document.getElementById("name").value;
 
-    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Deepavali%0Awww.google.com';
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Deepavali%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/deepavali.html';
 }
