668dfe7f3172f483a44fac42890b39d54603b7b7
limshouzheng <limshouzheng@gmail.com>
2020-10-25 12:15:33 +0800

    ChangeJS2

--- send.js
+++ send.js
@@ -1,23 +1,23 @@
 function sendRayaWhatsapp(){
     inputName = document.getElementById("name").value;
 
-    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Selamat%20Hari%20Raya%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/hariraya.html';
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Selamat%20Hari%20Raya%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/hariraya.html%0A%0AChoose another greeting card,please click here-->%20https://shouzhengzheng.github.io/Individual_Assignment/Index.html';
 }
 
 function sendCNYWhatsapp(){
     inputName = document.getElementById("name").value;
 
-    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Chinese%20New%20Year!%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/happynewyear.html';
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Chinese%20New%20Year!%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/happynewyear.html%0A%0AChoose another greeting card,please click here-->%20https://shouzhengzheng.github.io/Individual_Assignment/Index.html';
 }
 
 function sendChristmasWhatsapp(){
     inputName = document.getElementById("name").value;
 
-    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Merry%20Christmas%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/christmas.html';
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Merry%20Christmas%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/christmas.html%0A%0AChoose another greeting card,please click here-->%20https://shouzhengzheng.github.io/Individual_Assignment/Index.html';
 }
 
 function sendDeepavaliWhatsapp(){
     inputName = document.getElementById("name").value;
 
-    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Deepavali%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/deepavali.html';
+    window.location = 'http://wa.me/?text='+inputName+' wish%20you%20Happy%20Deepavali%0Ahttps://shouzhengzheng.github.io/Individual_Assignment/deepavali.html%0A%0AChoose another greeting card,please click here-->%20https://shouzhengzheng.github.io/Individual_Assignment/Index.html';
 }
